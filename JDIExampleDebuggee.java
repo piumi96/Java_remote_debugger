d7cd0b44684aff5e0a419f526ac8fe0ee3a20060
Piumi96 <piumidp96@gmail.com>
2020-11-02 10:52:08 +0530

    Created debugger and debuggee nodes

---
+++ JDIExampleDebuggee.java
@@ -0,0 +1,24 @@
+public class JDIExampleDebuggee {
+    public static void main(String[] args) {
+        
+        int a = 5;
+        int b = 10;
+        
+
+        int total = a + b;
+
+        add();
+
+        System.out.println(total);
+
+
+    }
+
+    public static void add(){
+        int a2 = 5;
+        int b2 = 10;
+
+        int total2 = a2 + b2;
+        System.out.println(total2);
+    }
+}
---
+++ JDIExampleDebugger.java
@@ -0,0 +1,120 @@
+import com.sun.jdi.Bootstrap;
+import com.sun.jdi.VirtualMachine;
+import com.sun.jdi.connect.Connector;
+import com.sun.jdi.connect.LaunchingConnector;
+import com.sun.jdi.request.ClassPrepareRequest;
+import com.sun.jdi.AbsentInformationException;
+import com.sun.jdi.ClassType;
+import com.sun.jdi.IncompatibleThreadStateException;
+import com.sun.jdi.LocalVariable;
+import com.sun.jdi.Location;
+import com.sun.jdi.StackFrame;
+import com.sun.jdi.VMDisconnectedException;
+import com.sun.jdi.Value;
+import com.sun.jdi.event.BreakpointEvent;
+import com.sun.jdi.event.ClassPrepareEvent;
+import com.sun.jdi.event.Event;
+import com.sun.jdi.event.EventSet;
+import com.sun.jdi.event.LocatableEvent;
+import com.sun.jdi.event.StepEvent;
+import com.sun.jdi.request.BreakpointRequest;
+import com.sun.jdi.request.StepRequest;
+
+import java.util.Map;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+public class JDIExampleDebugger {
+    private Class debugClass;
+    private int[] breakPointLines;
+
+    public Class getDebugClass() {
+        return debugClass;
+    }
+
+    public void setDebugClass(Class debugClass) {
+        this.debugClass = debugClass;
+    }
+
+    public int[] getBreakPointLines() {
+        return breakPointLines;
+    }
+
+    public void setBreakPointLines(int[] breakPointLines) {
+        this.breakPointLines = breakPointLines;
+    }
+
+    public VirtualMachine connectAndLaunchVM() throws Exception {
+
+        LaunchingConnector launchingConnector = Bootstrap.virtualMachineManager()
+                .defaultConnector();
+        Map<String, Connector.Argument> arguments = launchingConnector.defaultArguments();
+        arguments.get("main").setValue(debugClass.getName());
+        return launchingConnector.launch(arguments);
+    }
+
+    public void enableClassPrepareRequest(VirtualMachine vm) {
+        ClassPrepareRequest classPrepareRequest = vm.eventRequestManager().createClassPrepareRequest();
+        classPrepareRequest.addClassFilter(debugClass.getName());
+        classPrepareRequest.enable();
+    }
+
+    public void setBreakPoints(VirtualMachine vm, ClassPrepareEvent event) throws AbsentInformationException {
+        ClassType classType = (ClassType) event.referenceType();
+        for(int lineNumber: breakPointLines) {
+            Location location = classType.locationsOfLine(lineNumber).get(0);
+            BreakpointRequest bpReq = vm.eventRequestManager().createBreakpointRequest(location);
+            bpReq.enable();
+        }
+    }
+
+    public void displayVariables(LocatableEvent event) throws IncompatibleThreadStateException, 
+AbsentInformationException 
+    {
+        StackFrame stackFrame = event.thread().frame(0);
+        if(stackFrame.location().toString().contains(debugClass.getName())) {
+            Map<LocalVariable, Value> visibleVariables = stackFrame
+            .getValues(stackFrame.visibleVariables());
+            System.out.println("Variables at " + stackFrame.location().toString() +  " > ");
+            for (Map.Entry<LocalVariable, Value> entry : visibleVariables.entrySet()) {
+                System.out.println(entry.getKey().name() + " = " + entry.getValue());
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Debugger Start. . . ");
+
+        JDIExampleDebugger debuggerInstance = new JDIExampleDebugger();
+        debuggerInstance.setDebugClass(JDIExampleDebuggee.class);
+        int[] breakPoints = {8, 12, 19, 22};
+        debuggerInstance.setBreakPointLines(breakPoints);
+        VirtualMachine vm = null;
+        
+        try {
+            vm = debuggerInstance.connectAndLaunchVM();
+            debuggerInstance.enableClassPrepareRequest(vm);
+            EventSet eventSet = null;
+            while ((eventSet = vm.eventQueue().remove()) != null) {
+                for (Event event : eventSet) {
+                    if (event instanceof ClassPrepareEvent) {
+                        debuggerInstance.setBreakPoints(vm, (ClassPrepareEvent)event);
+                    }
+                    if (event instanceof BreakpointEvent) {
+                        debuggerInstance.displayVariables((BreakpointEvent) event);
+                    }
+                    vm.resume();
+                }
+            }
+        } catch (VMDisconnectedException e) {
+            System.out.println("Virtual Machine is disconnected.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
